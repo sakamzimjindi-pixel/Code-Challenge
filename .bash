868da042e0e0abc03be235f11d2d5b1bd601cce1
sakamzimjindi-pixel <sakamzimjindi@gmail.com>
2026-02-22 17:30:43 +0200

    Set AWS credentials and configuration in .bash
    Add AWS environment variables for configuration

---
+++ .bash
@@ -0,0 +1,6 @@
+#easiest way is to use aws configure
+
+export AWS_ACCESS_KEY_ID="AKIAYH57YDEWMHW2ESH2"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_BUCKET="s3://cct-ds-code-challenge-input-data/"
+export AWS_REGION="af-south-1"
